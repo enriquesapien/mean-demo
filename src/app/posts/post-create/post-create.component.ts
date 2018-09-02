f599f561ded2142eaa7789177c9b40d5d64825f2
Enrique Sapien <esapien@hotmail.com>
2018-09-01 21:17:40 -0500

    Add create and list post functionality using event and property binding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,13 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
 
-  enteredValue = '';
-
-  newPost = 'NO CONTENT';
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+
+    this.postCreated.emit(post);
+
   }
 
 }
