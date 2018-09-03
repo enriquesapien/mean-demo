fc6c48d9aef3bda116d6d0b72e6c3e3d350bae23
Enrique Sapien <esapien@hotmail.com>
2018-09-03 00:16:27 -0500

    Add CORS management in Express middleware configuration

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,22 @@ const express = require('express');
 // create the express app
 const app = express();
 
+// CORS management middleware
+app.use((req, res, next) => {
+  // allow clients from any other domain
+  // e.g. localhost:4200 (Angular) -> localhost:3000 (Node/Express)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+})
+
+// first argument(s) (like the path) are filters
+// last argument is default
 app.use('/api/posts', (req, res, next) => {
 
   const posts = [
