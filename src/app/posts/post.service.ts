30135acf290c3911791dfc2981b37412e03571ab
Enrique Sapien <esapien@hotmail.com>
2018-09-03 21:50:01 -0500

    Transform fetched data to match 'id' property

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
@@ -13,9 +14,22 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{ message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe((postData) => {
-      this.posts = postData.posts;
+    this.http
+    .get<{ message: string, posts: any }>(
+      'http://localhost:3000/api/posts'
+    )
+    .pipe(map((postData) => {
+      // transform each post element in the array
+      return postData.posts.map(post => {
+        return {
+          id: post._id,
+          title: post.title,
+          content: post.content
+        };
+      });
+    }))
+    .subscribe(transformedPosts => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     });
 
