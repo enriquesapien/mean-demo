3ec56fc4405a1fb0168fe525ba74cb2037a35ee3
Enrique Sapien <esapien@hotmail.com>
2018-09-04 00:00:11 -0500

    Add generated ID to created post

--- backend/app.js
+++ backend/app.js
@@ -49,15 +49,12 @@ app.post('/api/posts/', (req, res, next) => {
   });
 
   // save to DB
-  post.save();
-
-  // const post = req.body;
-  console.log(post);
-
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
-
 });
 
 // FETCH POSTS
@@ -78,8 +75,8 @@ app.get('/api/posts', (req, res, next) => {
 app.delete('/api/posts/:id', (req, res, next) => {
 
   Post.deleteOne({ _id: req.params.id })
-  .then(result => {
-    console.log(result);
+  .then(createdPost => {
+    console.log(createdPost);
     res.status(200).json({
       message: 'Post deleted!'
     });
