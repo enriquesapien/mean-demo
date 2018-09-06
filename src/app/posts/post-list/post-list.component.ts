c9c260160c31805e4766fa43011e7f93b8f072f7
Enrique Sapien <esapien@hotmail.com>
2018-09-05 22:25:03 -0500

    Add paginator component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
 import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-post-list',
@@ -18,6 +19,9 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postService: PostService) {}
@@ -33,10 +37,13 @@ export class PostListComponent implements OnInit, OnDestroy {
       });
   }
 
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
+  }
+
   onDelete(postId: string) {
     this.postService.deletePost(postId);
   }
-
 
   ngOnDestroy(): void {
     this.postsSub.unsubscribe();
